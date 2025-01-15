9a867837d72cf2fe5039880fe8cf735f5f0acc43
Piotr Okrutniak <piotrokrutniak@gmail.com>
2025-01-15 15:09:32 +0100

    Adjust layout paddings

--- src/components/layouts/PageContentSidebarLayout.tsx
+++ src/components/layouts/PageContentSidebarLayout.tsx
@@ -4,7 +4,7 @@ export const PageContentSidebarLayout = ({
   children: React.ReactNode;
 }) => {
   return (
-    <section className="flex flex-1 gap-16 mx-auto w-full min-h-screen pt-16 px-3 max-w-7xl">
+    <section className="flex flex-1 gap-16 mx-auto w-full min-h-screen py-8 px-3 max-w-7xl">
       {children}
     </section>
   );
--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,6 +1,6 @@
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-1 flex-col w-full min-h-screen pt-16 items-center">
+    <main className="flex flex-1 flex-col w-full min-h-screen pt-[60px] items-center">
       {children}
     </main>
   );
